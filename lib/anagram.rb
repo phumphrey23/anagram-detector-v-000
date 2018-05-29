02f315e50358c673f3a6c1e6e19c5d677aea0907
Precious Humphrey <phumphrey23@gmail.com>
2018-05-29 01:39:25 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(words)
+   words.find_all do |x| 
+     compare(x)
+   end
+ end
+ 
+def compare
+   word.chars.to_a.sort == self.chars.to_a.sort
+end
+
+end
